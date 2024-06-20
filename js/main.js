0a0a46213b66663a111c3aa732b8f66a76b91016
KarolinaR98 <64532044+KarolinaR98@users.noreply.github.com>
2024-06-20 12:13:26 +0200

    jQuery project update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.hamburger').click(function(){
+        $('.main-menu').toggleClass('open');
+    });
+
+    $('.main-menu-item a').on('click', function(e){
+        e.preventDefault();
+
+        $('body').fadeOut(2000, function(){
+            window.location.href = $(e.target).attr('href');
+        });
+    })
+
+});
